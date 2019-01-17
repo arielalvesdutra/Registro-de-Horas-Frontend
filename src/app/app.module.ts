9d283fe760e72ddf6299af2b131406298348c5a0
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-17 10:40:30 -0200

    Adiciona layout básico para o Time Recorder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimeRecorderComponent } from './time-recorder/time-recorder.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeRecorderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/time-recorder/time-recorder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-recorder',
+  templateUrl: './time-recorder.component.html',
+  styleUrls: ['./time-recorder.component.css']
+})
+export class TimeRecorderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
