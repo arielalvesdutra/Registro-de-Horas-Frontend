ce1e537a7d4ca485a7e56c4a896c6dfcd0b8932e
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-18 05:08:30 -0200

    Tentei usar o flexbox com angular, mas o angular buga, entao vou para o bootstrap ou css puro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// import { FlexModule, FlexStyleBuilder, FlexLayoutModule } from '@angular/flex-layout'
+
 import { AppComponent } from './app.component';
 import { TimeRecorderComponent } from './time-recorder/time-recorder.component';
+import { RecorderComponent } from './recorder/recorder.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeRecorderComponent
+    TimeRecorderComponent,
+    RecorderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FlexModule,
+    // FlexLayoutModule
+    // FlexLayoutModule
+    // FlexStyleBuilder
   ],
   providers: [],
   bootstrap: [AppComponent]
