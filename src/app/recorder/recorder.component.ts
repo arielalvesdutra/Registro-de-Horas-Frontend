c87ee5158579dd5d8313fae1d123e903c5d5574e
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-18 18:12:35 -0200

    Adiciona funcionalidade para pegar data, inicio ou fim, e exibir na tela

--- src/app/recorder/recorder.component.ts
+++ src/app/recorder/recorder.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDate } from '@angular/common'
 
 @Component({
   selector: 'app-recorder',
@@ -9,14 +10,31 @@ export class RecorderComponent implements OnInit {
 
   constructor() { }
 
+  recordStarted: boolean = false
+
+  recordTitle : string
+  recordInit: string
+  recordEnd: string
+
   ngOnInit() {
   }
 
-  startTimer() {
+  startTimer(recordName: string) {
+
+    if (!recordName)  {
+      console.log('não tem texto...')
+      return false
+    }
+
+    this.recordStarted = true
+    this.recordTitle = recordName
+    this.recordInit = formatDate(new Date(), 'yyyy/MM/dd HH:mm', 'en')
     console.log(`Iniciando o temporizador...`)
   }
 
   stopTimer() {
+    this.recordStarted = false
+    this.recordEnd = formatDate(new Date(), 'yyyy/MM/dd HH:mm', 'en')
     console.log(`Parando o temporizador...`)
   }
 }
