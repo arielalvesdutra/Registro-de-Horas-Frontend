2a09d9c8198e1526921544572246dd75a619d963
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 17:32:58 -0200

    Adiciona componente de listagem de registros

---
+++ src/app/time-record-listing/time-record-listing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-time-record-listing',
+  templateUrl: './time-record-listing.component.html',
+  styleUrls: ['./time-record-listing.component.css']
+})
+export class TimeRecordListingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
