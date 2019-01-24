780e1c102241033160b54a02723885836d7e2ef9
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-24 17:32:28 -0200

    Adiciona importação do modulo HTTP e Remove comentários desnecessários

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { FormsModule} from '@angular/forms'
-
-// import { FlexModule, FlexStyleBuilder, FlexLayoutModule } from '@angular/flex-layout'
 
 import { AppComponent } from './app.component';
 import { TimeRecorderComponent } from './time-recorder/time-recorder.component';
 import { RecorderComponent } from './recorder/recorder.component';
+import { TimeRecordListingComponent } from './time-record-listing/time-record-listing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TimeRecorderComponent,
-    RecorderComponent
+    RecorderComponent,
+    TimeRecordListingComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-    // FlexModule,
-    // FlexLayoutModule
-    // FlexLayoutModule
-    // FlexStyleBuilder
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
