d61735ef883b113261660d0707b67b2bfa9cf762
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-01-30 16:38:38 -0200

    Adiciona serviço para integrar o backend com o frontend

---
+++ src/app/records.service.ts
@@ -0,0 +1,49 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http'
+import { Observable, throwError } from 'rxjs'
+import { catchError, retry } from 'rxjs/operators'
+
+import { TimeRecord } from './time-record.model'
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecordsService {
+
+  backendApi = 'http://192.168.11.100:8000/'
+  
+  static registroAdicionado = new EventEmitter<string>()
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('Um erro ocorreu:', error.error.message);
+    } else {
+      console.error(
+        `Status de erro retornado do Backend: ${error.status}, ` +
+        `O corpo estava: ${error.error}`);
+    }
+    return throwError(
+      'Algo errado ocorreu. Favor acessar mais tarde.');
+  }
+
+  constructor(
+    private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+  
+  addRecord(timeRecord: TimeRecord) {
+    console.log('addRecord...')
+    return this.http.post<TimeRecord>(this.backendApi + 'addRecord', timeRecord, httpOptions)
+  }
+
+  getRecords(): Observable<TimeRecord[]> {
+    return this.http.get<TimeRecord[]>(this.backendApi + 'getRecords')
+  }
+}
