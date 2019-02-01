31ffa872c7930c5e5ca3e3f92bb28367baad7e1f
Ariel Alves Dutra <alves.lsn@gmail.com>
2019-02-01 11:00:10 -0200

    Adiciona métodos para e atributos para atualizar um registro, Adiciona métodos e atributos para filtras os registros

--- src/app/time-record-listing/time-record-listing.component.ts
+++ src/app/time-record-listing/time-record-listing.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { RecordsService } from '../records.service'
 import { TimeRecord } from '../time-record.model'
+import { filter } from 'rxjs/operators';
+import { ifError } from 'assert';
 
 @Component({
   selector: 'app-time-record-listing',
@@ -10,23 +12,111 @@ import { TimeRecord } from '../time-record.model'
 })
 export class TimeRecordListingComponent implements OnInit {
 
+  dateFilter: string
+  titleFilter: string
+
   records: TimeRecord[]
-    
-  constructor(private recordService: RecordsService) {}
+
+  recordEditId: number
+  recordTitleToUpdate: string
+  recordInitDateToUpdate: string
+  recordEndDateToUpdate: string
+
+  showFilters: boolean = false
+
+  constructor(private recordService: RecordsService) { }
 
   ngOnInit() {
     this.getRecords()
-    
-    RecordsService.registroAdicionado.subscribe(param => this.getRecords())
+
+    RecordsService.registroAdicionado.subscribe(param => this.getRecordsByFilters())
+  }
+
+  private arrayIsEmpty(array: any[]): boolean {
+    for (let index in array) {
+      return false
+    }
+
+    return true
+  }
+
+  private buildFilterUrl(filterOptions: string[]) {
+
+    if (this.arrayIsEmpty(filterOptions)) {
+      return ''
+    }
+
+    let filterUrl = 'filters?'
+    let firstElement: boolean = true
+
+    for (let filter in filterOptions) {
+      if (firstElement) {
+        filterUrl += `${filter}=${filterOptions[filter]}`
+        firstElement = false
+      } else {
+        filterUrl += `&${filter}=${filterOptions[filter]}`
+      }
+    }
+
+    return filterUrl
+  }
+
+  cancelEdit() {
+    this.recordEditId = null
+  }
+
+  cleanRecordToUpdateValues() {
+    this.recordEditId = null
+    this.recordTitleToUpdate = null
+    this.recordInitDateToUpdate = null
+    this.recordEndDateToUpdate = null
+  }
+
+  confirmUpdate(timeRecord: TimeRecord) {
+    this.recordService.updateRecord(new TimeRecord(
+      this.recordEditId,
+      this.recordTitleToUpdate,
+      this.recordInitDateToUpdate,
+      this.recordEndDateToUpdate
+    ))
+      .subscribe(() => this.getRecords())
+
+    this.cleanRecordToUpdateValues()
   }
 
   deleteRecord(id: number): void {
     this.recordService.deleteRecord(id)
       .subscribe(() => this.getRecords())
   }
- 
-  getRecords() {
-    this.recordService.getRecords()
+
+  getRecords(filtersUrl: string = '') {
+    this.recordService.getRecords(filtersUrl)
       .subscribe(data => this.records = data)
+  }
+
+  getRecordsByFilters() {
+    let filterOptions = []
+
+    if (this.dateFilter) {
+      filterOptions['initDate'] = this.dateFilter
+    }
+    if (this.titleFilter) {
+      filterOptions['title'] = this.titleFilter
+    }
+
+    let filtersUrl = this.buildFilterUrl(filterOptions)
+
+    this.getRecords(filtersUrl)
+  }
+
+  showFilterToggle() {
+    this.showFilters = !this.showFilters
+  }
+
+  updateRecord(timeRecord: TimeRecord) {
+    this.recordEditId = timeRecord.id
+    this.recordTitleToUpdate = timeRecord.title
+    this.recordInitDateToUpdate = timeRecord.initDate
+    this.recordEndDateToUpdate = timeRecord.endDate
   }
 }
